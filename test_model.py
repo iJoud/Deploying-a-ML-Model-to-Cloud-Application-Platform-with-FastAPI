e10127a867de8ba52e1bcd63a9a2d39b944c1089
iJoud <jood.ghamdi1@gmail.com>
2024-05-18 02:38:21 +0300

    prepare for submission

--- test_model.py
+++ test_model.py
@@ -1,11 +1,12 @@
 import pytest
 import pandas as pd
 import numpy as np
+import os
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import ExtraTreesClassifier
-from ml.model import train_model, compute_model_metrics, inference
 from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference, performance_on_data_slices
 
 
 @pytest.fixture(scope="session")
@@ -60,31 +61,60 @@ def test_train_model(train_test_data):
     assert isinstance(model, ExtraTreesClassifier)
 
 
-def test_inference(train_test_data):
+# def test_inference(train_test_data):
 
-    model_path = 'model/model.pkl'
+#     model_path = 'model/model.pkl'
 
-    _, _, X_test, y_test = train_test_data
+#     _, _, X_test, y_test = train_test_data
 
-    model = pickle.load(open(model_path, 'rb'))
-    preds = inference(model, X_test)
+#     model = pickle.load(open(model_path, 'rb'))
+#     preds = inference(model, X_test)
     
-    assert isinstance(preds, np.ndarray)
-    assert len(preds) == len(y_test)
+#     assert isinstance(preds, np.ndarray)
+#     assert len(preds) == len(y_test)
+
+def test_performance_on_data_slices(data):
+    model_path = 'model'
+
+    model_folder_path = './model/'
+
+    with open(os.path.join(model_folder_path, 'model.pkl'), 'rb') as f:
+        model = pickle.load(f)
+
+    with open(os.path.join(model_folder_path, 'lb.pkl'), 'rb') as f:
+        lb = pickle.load(f)
+
+    with open(os.path.join(model_folder_path, 'encoder.pkl'), 'rb') as f:
+        encoder = pickle.load(f)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    output_file_path = performance_on_data_slices(model, data, cat_features, encoder, lb)
+
+    assert isinstance(output_file_path, str)
 
 
-def test_compute_model_metrics(train_test_data):
+# def test_compute_model_metrics(train_test_data):
 
-    model_path = 'model/model.pkl'
+#     model_path = 'model/model.pkl'
 
-    _, _, X_test, y_test = train_test_data
+#     _, _, X_test, y_test = train_test_data
 
-    model = pickle.load(open(model_path, 'rb'))
+#     model = pickle.load(open(model_path, 'rb'))
 
-    preds = inference(model, X_test)
+#     preds = inference(model, X_test)
 
-    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+#     precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
-    assert isinstance(precision, float)
-    assert isinstance(recall, float)
-    assert isinstance(fbeta, float)
+#     assert isinstance(precision, float)
+#     assert isinstance(recall, float)
+#     assert isinstance(fbeta, float)
